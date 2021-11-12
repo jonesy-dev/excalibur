b357c5fd3e7a012fba4fd7aa4c1ea6300ca225a1
Andrea Falzi <andrea.falzi1988@gmail.com>
2021-11-12 12:53:19 +0100

    template version 1

--- js/index.js
+++ js/index.js
@@ -2,14 +2,18 @@
 //Left to right scroll pages
 const articleTitleArray = document.querySelectorAll(".article-title");
 const articleTitleSingle = document.querySelector(".article-title");
+const articleSlider = document.querySelector(".article-slider");
+const section = document.querySelector("section");
 
 /********** FUNCTIONS **********/
 //Function that automatically add "article-title-" class for each element you add inside the ARTICLE-SLIDER to avoid repetion in the code and assign to that a property left
 articleTitleArray.forEach((articleTitleSingle, i) => {
   i = 1 + i;
   articleTitleSingle.classList.add("article-title-" + i);
-  document.querySelector(".article-title-" + i).style.left = (i - 1) * 98 + "px";
+  document.querySelector(".article-title-" + i).style.left = (i - 1) * articleTitleSingle.scrollWidth + "px";
 });
+
+section.style.width = section.scrollWidth + "px"
 
 /********** EXTERNAL FUNCTIONS **********/
 const scrollContainer = document.querySelector(".main-article-template");
