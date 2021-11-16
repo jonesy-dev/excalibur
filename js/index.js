12556e4cc779d49dd5eb43f4f8752fa5cd547d5a
Andrea Falzi <andrea.falzi1988@gmail.com>
2021-11-16 14:07:29 +0100

    template v2

--- js/index.js
+++ js/index.js
@@ -6,83 +6,66 @@ const articleContentArray = document.querySelectorAll(".article-content");
 const articleContentSingle = document.querySelector(".article-content");
 const articleSlider = document.querySelector(".article-slider");
 const section = document.querySelector("section");
+const widthTitlePlusContent = document.querySelector(".article-title").offsetWidth + document.querySelector(".article-content").offsetWidth;
 
 /********** FUNCTIONS **********/
 //Function that automatically add "article-title-" class for each element you add inside the ARTICLE-SLIDER to avoid repetion in the code and assign to that a property left
 articleTitleArray.forEach((articleTitleSingle, i) => {
   i = 1 + i;
   articleTitleSingle.classList.add("article-title-" + i);
+  articleTitleSingle.addEventListener("click", () => {
+    window.scroll((articleSlider.offsetLeft - widthTitlePlusContent + (widthTitlePlusContent * (i - 1))), 0);
+  });
   document.querySelector(".article-title-" + i).style.opacity = 1;
   document.querySelector(".article-title-" + i).style.transition = "opacity 400ms ease-out";
-  // document.querySelector(".article-title-" + i).style.left = (i - 1) * articleTitleSingle.scrollWidth + "px";
 });
 
 articleContentArray.forEach((articleContentSingle, i) => {
   i = 1 + i;
   articleContentSingle.classList.add("article-content-" + i);
+  articleContentSingle.id = "article" + i;
   document.querySelector(".article-content-" + i).style.opacity = 1;
   document.querySelector(".article-content-" + i).style.transition = "opacity 400ms ease-out";
-  // document.querySelector(".article-title-" + i).style.left = (i - 1) * articleTitleSingle.scrollWidth + "px";
 });
 
 //dynamic section width
 section.style.width = section.scrollWidth + "px";
 
-// /* WIDTH CALCULATOR */
-window.addEventListener("scroll", function () {
-  console.log(window.scrollX);
-});
+// // /* WIDTH CALCULATOR */
+// window.addEventListener("scroll", function () {
+//   console.log(window.pageXOffset);
+// });
 
 //Minimaze title
 window.addEventListener("scroll", function () {
-  const articleSlider = document.querySelector(".article-slider");
-  const articleTitle = document.querySelector(".article-title");
-  const articleContent = document.querySelector(".article-content");
+  const articleSliderHeight = articleSlider.offsetHeight;
+  let itemXCol = 5;
 
-  if (window.scrollX >= articleSlider.offsetLeft - articleContent.offsetWidth) {
-    document.querySelector(".article-title-1").style.opacity = 1;
-    document.querySelector(".article-content-1").style.opacity = 0;
-    document.querySelector(".article-title-1").style.height = 20 + "px";
-    document.querySelector(".article-title-1").style.position = "sticky";
-    document.querySelector(".article-title-1").style.left = 0;
-    document.querySelector(".article-title-1").style.top = 0;
-
-    if (window.scrollX >= articleSlider.offsetLeft + 600 - articleContent.offsetWidth) {
-      document.querySelector(".article-title-2").style.opacity = 1;
-      document.querySelector(".article-content-2").style.opacity = 0;
-      document.querySelector(".article-title-2").style.height = 20 + "px";
-      document.querySelector(".article-title-2").style.position = "sticky";
-      document.querySelector(".article-title-2").style.left = 0;
-      document.querySelector(".article-title-2").style.top = 20 + "px";
-
-      if (window.scrollX >= articleSlider.offsetLeft + 1200 - articleContent.offsetWidth) {
-        document.querySelector(".article-title-3").style.opacity = 1;
-        document.querySelector(".article-content-3").style.opacity = 0;
-        document.querySelector(".article-title-3").style.height = 20 + "px";
-        document.querySelector(".article-title-3").style.position = "sticky";
-        document.querySelector(".article-title-3").style.left = 0;
-        document.querySelector(".article-title-3").style.top = 40 + "px";
-      } else {
-        document.querySelector(".article-content-3").style.opacity = null;
-        document.querySelector(".article-title-3").style.height = null;
-        document.querySelector(".article-title-3").style.position = null;
-        document.querySelector(".article-title-3").style.left = null;
-        document.querySelector(".article-title-3").style.top = null;
+  for (let i = 1; i <= articleTitleArray.length; i++) {
+    if (window.pageXOffset >= articleSlider.offsetLeft + (i - 1) * widthTitlePlusContent - articleContentSingle.offsetWidth) {
+      document.querySelector(".article-content-" + i).style.opacity = 0;
+      document.querySelector(".article-title-" + i).style.height = articleSliderHeight / itemXCol + "px";
+      document.querySelector(".article-title-" + i).style.position = "sticky";
+      if (i <= itemXCol) {
+        document.querySelector(".article-title-" + i).style.left = 0;
+        document.querySelector(".article-title-" + i).style.top = ((i - 1) * articleSliderHeight) / itemXCol + "px";
+      } else if (i <= itemXCol * 2) {
+        document.querySelector(".article-title-" + i).style.left = 100 + "px";
+        document.querySelector(".article-title-" + i).style.top = ((i - itemXCol - 1) * articleSliderHeight) / itemXCol + "px";
+      } else if (i <= itemXCol * 3) {
+        document.querySelector(".article-title-" + i).style.left = 200 + "px";
+        document.querySelector(".article-title-" + i).style.top = ((i - itemXCol * 2 - 1) * articleSliderHeight) / itemXCol + "px";
+      } else if (i <= itemXCol * 4) {
+        document.querySelector(".article-title-" + i).style.left = 300 + "px";
+        document.querySelector(".article-title-" + i).style.top = ((i - itemXCol * 3 - 1) * articleSliderHeight) / itemXCol + "px";
       }
     } else {
-      document.querySelector(".article-content-2").style.opacity = null;
-      document.querySelector(".article-title-2").style.height = null;
-      document.querySelector(".article-title-2").style.position = null;
-      document.querySelector(".article-title-2").style.left = null;
-      document.querySelector(".article-title-2").style.top = null;
+      document.querySelector(".article-content-" + i).style.opacity = null;
+      document.querySelector(".article-title-" + i).style.height = null;
+      document.querySelector(".article-title-" + i).style.position = null;
+      document.querySelector(".article-title-" + i).style.left = null;
+      document.querySelector(".article-title-" + i).style.top = null;
     }
-  } else {
-    
-    document.querySelector(".article-content-1").style.opacity = null;
-    document.querySelector(".article-title-1").style.height = null;
-    document.querySelector(".article-title-1").style.position = null;
-    document.querySelector(".article-title-1").style.left = null;
-    document.querySelector(".article-title-1").style.top = null;
   }
 });
 
@@ -93,7 +76,7 @@ document.addEventListener("wheel", function (e) {
     return;
   }
   let delta = (e.deltaY || -e.wheelDelta || e.detail) >> 10 || 1;
-  delta = delta * -80;
+  delta = delta * - 300;
   document.documentElement.scrollLeft -= delta;
   // safari needs also this
   document.body.scrollLeft -= delta;
